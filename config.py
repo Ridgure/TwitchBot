04e9859da16ae84d662f70db99b75c44bb6256f1
Ridgure <ridgure@gmail.com>
2018-02-18 06:48:48 +0100

    Fidelidee
    Kan jeg endelig legge til flere filer?

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# bot.py
+
+HOST = "irc.twitch.tv"                                  # the Twitch IRC server
+PORT = 6667                                             # always use port 6667!
+NICK = "riboture"                                       # your Twitch username, lowercase
+PASS = "oauth:8qw7v2bn8pa8r69q53k91ycv9u4vi3"           # your Twitch OAuth token
+CHAN = "#ridgure"                                       # the channel you want to join
+RATE = 20/30                                            # messages per second
+
+User_ID_ridgure = "106586349"
+User_ID_riboture = "109949586"
+Client_ID = "7cvp1bezng3ypb8bosyzosl3fcgb5ra"
+Client_Secret = "gf4y8h01lffer7w0msjcnakdbstlfv"

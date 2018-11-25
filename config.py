13c05412d8af5e513bcc5ac16bb1ebb3324ce7d5
Ridgure <ridgure@gmail.com>
2018-11-26 10:44:23 +1300

    TEST

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ PORT = 6667                                             # always use port 6667!
 NICK = "riboture"                                       # your Twitch username, lowercase
 PASS = "oauth:8qw7v2bn8pa8r69q53k91ycv9u4vi3"           # your Twitch OAuth token
 CHAN = "#ridgure"                                       # the channel you want to join
-RATE = 20/30                                            # messages per second
 
 User_ID_ridgure = "106586349"
 User_ID_riboture = "109949586"
